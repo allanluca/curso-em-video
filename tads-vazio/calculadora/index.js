870fb779e716286cfd86d3c516673a4457539a08
allanluca <allanlucas6999@gmail.com>
2021-08-06 11:58:56 -0300

    commit full course

---
+++ tads-vazio/calculadora/index.js
@@ -0,0 +1,16 @@
+alert("Bem vindo! calculadora web.ATC-1");
+function limpar(){
+    document.getElementById('visor').value = '';
+
+}
+function unir(valor){
+    document.getElementById('visor').value+= valor;
+
+}
+
+function calcular (){
+    var resultado = 0;
+    resultado = document.getElementById('visor').value;
+    document.getElementById('visor').value = eval(resultado);
+
+}
